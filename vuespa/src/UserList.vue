358ca0b94e563d3c35b5066b542a8bf30916ead7
ekdev01 <31770879+ekdev01@users.noreply.github.com>
2018-10-07 01:46:10 +0200

    Add and update template, script and style blocks.

---
+++ vuespa/src/UserList.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="user in data">
+                <user-details v-bind:usr="user" v-on:accept="registerUser(user)"></user-details>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {  
+        name: 'user-list',
+        components: {
+            'user-details': UserDetails
+        },
+        props: ['data', 'usersComing'],
+        methods: {
+            registerUser: function (user) {
+                console.log(user);
+                this.usersComing.push(user);
+                console.log(this.usersComing);
+            }
+        },
+        data: function() {
+            return usersWhoWillCome: []
+        }
+    }
+</script>
+
+<!-- Optional -->
+<style></style>
