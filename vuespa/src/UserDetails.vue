6a559653080ebd92ca2516ddd5d18448b14a1abb
ekdev01 <31770879+ekdev01@users.noreply.github.com>
2018-10-07 02:05:59 +0200

    Add and Update UserDetails component

---
+++ vuespa/src/UserDetails.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        id: {{usr.id}}, ville: {{usr.address.city}}, email: {{usr.email}} 
+        <button v-on:click="acceptInvitation">Accept invit</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'user-detials',
+        props: ['usr'],
+        methods: {
+            // Permet d'emettre un event avec $emit
+            acceptInvitation: function () {
+                this.$emit('accept');
+            }
+        }
+    }
+</script>
+
+<style>
+    
+</style>
