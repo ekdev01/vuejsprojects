ef6e1d13d47e434667dcd3ea9f9ed1e13461cd3d
ekdev01 <31770879+ekdev01@users.noreply.github.com>
2018-10-07 03:23:58 +0200

    Add routes with vue-rooter library

--- vuespa/src/main.js
+++ vuespa/src/main.js
@@ -1,5 +1,19 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import UserList from './UserList.vue';
+import UserList from './UserDetails.vue';
+
 import App from './App.vue'
+
+
+Vue.use(VueRouter);
+
+// Nos routes
+const routes = [
+  { path: '/', redirect: '/users'}, // http://localhost:8082/
+  { path: '/users', component: UserList}, // http://localhost:8082/users
+  { path: '/users/:id', component: UserDetails} // http://localhost:8082/users/1
+];
 
 new Vue({
   el: '#app',
