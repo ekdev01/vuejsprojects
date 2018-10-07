2152c3e61619456ddc347a76a6f6f43e8444e5e8
ekdev01 <31770879+ekdev01@users.noreply.github.com>
2018-10-07 03:53:44 +0200

    Create and update UserSummary component

---
+++ vuespa/src/UserSummary.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="user-summary">
+        id: {{usr.id}}, ville: {{usr.address.city}}, email: {{usr.email}} <br /> 
+        <button v-on:click="acceptInvitation">Accept invit</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'user-summary',
+        props: ['usr'],
+        methods: {
+            // Permet d'emettre un event avec $emit
+            acceptInvitation: function () {
+                this.$emit('accept');
+            }
+        }
+    }
+</script>
+
+<style>
+    .user-summary {
+        border: 2px solid grey;
+        margin: 10px;
+        padding: 15px;
+        width: 250px;
+        float: left;
+    }
+</style>
