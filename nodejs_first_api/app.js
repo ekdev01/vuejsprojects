6d1485948c7816cc10eb1a00fa7ca50980a31fda
ekdev01 <31770879+ekdev01@users.noreply.github.com>
2018-10-08 18:17:19 +0200

    Add query and app execution on port 8080

--- nodejs_first_api/app.js
+++ nodejs_first_api/app.js
@@ -1,3 +1,12 @@
 const express = require('express')
 const app = express()
 
+// Creation requete GET /api
+app.get('/api' . (res, req) => {
+    res.send('Root API')
+})
+
+// Execution de l'application sur le port 8080 avec 
+// en callback un message dans la console
+app.listen(8080, () => { console.log('Started on port 8080') })
+
