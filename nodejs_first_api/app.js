c6134265f71d431bd835fae18abf387f6b739da6
ekdev01 <31770879+ekdev01@users.noreply.github.com>
2018-10-08 18:59:24 +0200

    Add ane another path/url

--- nodejs_first_api/app.js
+++ nodejs_first_api/app.js
@@ -7,6 +7,10 @@ app.get('/api', (req, res) => {
     res.send('Root API')
 })
 
+app.get('/api/v1', (req, res) => {
+    res.send('API Version 1')
+})
+
 // Execution de l'application sur le port 8080 avec 
 // en callback un message dans la console
 app.listen(8080, () => { console.log('Started on port 8080') })
