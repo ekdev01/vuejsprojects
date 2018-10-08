47b4f38ecdc89de3d1bfb42cecec862c452df2fc
ekdev01 <31770879+ekdev01@users.noreply.github.com>
2018-10-09 01:25:26 +0200

    Nouvelle réécriture des routes/urls avec le middleware MembersRouter

--- nodejs_first_api/app.js
+++ nodejs_first_api/app.js
@@ -22,6 +22,9 @@ const members = [
     }
 ]
 
+// les routers ici 
+let MembersRouter = express.Router() // pour gerer la branche /members 
+
 // les middleware ici
 
 // middleware morgan pour le debuggage
@@ -31,106 +34,108 @@ app.use(morgan('dev'))
 app.use(bodyParser.json());                         // pour le parsing des reponse en application/json
 app.use(bodyParser.urlencoded({ extended: true })); // pour le parsing des donnes formulaire en application/x-www-form-urlencoded
 
-// pour la lecture d'un membre a partir de son id 
-app.get('/api/v1/members/:id', (req, res) => {
+// Appel au router MembersRouter
+MembersRouter.route('/:id')
 
-    let index = getIndex(req.params.id)
+    // pour la lecture d'un membre a partir de son id 
+    .get((req, res) => {
 
-    if(typeof(index) == 'string') {
-        res.json(error(index))
-    } else {
-        res.json(success(members[index]))
-    }
-})
+        let index = getIndex(req.params.id)
 
-// pour la mise a jour d'un membre a partir de son id
-app.put('/api/v1/members/:id', (req, res) => {
+        if(typeof(index) == 'string') {
+            res.json(error(index))
+        } else {
+            res.json(success(members[index]))
+        }
+    })
+    // pour la mise a jour d'un membre a partir de son id
+    .put((req, res) => {
 
-    let index = getIndex(req.params.id)
+        let index = getIndex(req.params.id)
 
-    if(typeof(index) == 'string') {
-        res.json(error(index))
-    } 
-    else {
-        let same = false
+        if(typeof(index) == 'string') {
+            res.json(error(index))
+        } 
+        else {
+            let same = false
 
-        // on verifie si nom deja pris
-        for(let i=0; i<members.length; i++) {
-            if(req.body.name == members[i].name && req.params.id != members[i].id) {
-                same = true
-                break
+            // on verifie si nom deja pris
+            for(let i=0; i<members.length; i++) {
+                if(req.body.name == members[i].name && req.params.id != members[i].id) {
+                    same = true
+                    break
+                }
+            }
+
+            if(same) {
+                res.json(error('same name'))
+            } else {
+                members[index].name = req.body.name
+                res.json(success(true))
             }
         }
 
-        if(same) {
-            res.json(error('same name'))
-        } else {
-            members[index].name = req.body.name
-            res.json(success(true))
+    })
+    // pour la suppression d'un membre a partir de son id
+    .delete((req, res) => {
+        let index = getIndex(req.params.id)
+
+        if(typeof(index) == 'string') {
+            res.json(error(index))
+        } 
+        else {
+            members.splice(index, 1)
+            res.json(success(members))
+            //res.json(success(true))
         }
-    }
+    })
 
-})
-
-
-// pour la suppression d'un membre a partir de son id
-app.delete('/api/v1/members/:id', (req, res) => {
-    let index = getIndex(req.params.id)
-
-    if(typeof(index) == 'string') {
-        res.json(error(index))
-    } 
-    else {
-        members.splice(index, 1)
-        res.json(success(members))
-        //res.json(success(true))
-    }
-})
-
-// pour la lecture de tous les membres
-app.get('/api/v1/members', (req, res) => {
+MembersRouter.route('/')
+    // pour la lecture de tous les membres
+    .get((req, res) => {
 
     if(req.query.max != undefined && req.query.max > 0) {
-        res.json(success(members.slice(0, req.query.max)))
-    } 
-    else if(req.query.max != undefined) {  // Cas d'erreur
-        res.json(error('Wrong max value'))
-    } 
-    else {
-        res.json(success(members))
-    }
-})
-
-// pour la creation d'un membre 
-app.post('/api/v1/members', (req, res) => {
-
-    if(req.body.name) {
-
-        let sameName = false
-
-        // on verifie si nom deja pris
-        for(let i=0; i<members.length; i++) {
-            if (members[i].name == req.body.name) {
-                sameName = true
-                break
-            }
+            res.json(success(members.slice(0, req.query.max)))
+        } 
+        else if(req.query.max != undefined) {  // Cas d'erreur
+            res.json(error('Wrong max value'))
+        } 
+        else {
+            res.json(success(members))
         }
+    })
+    // pour la creation d'un membre 
+    .post((req, res) => {
 
-        if(sameName) {
-            res.json(error('name already taken'))
+        if(req.body.name) {
+            let sameName = false
+
+            // on verifie si nom deja pris
+            for(let i=0; i<members.length; i++) {
+                if (members[i].name == req.body.name) {
+                    sameName = true
+                    break
+                }
+            }
+
+            if(sameName) {
+                res.json(error('name already taken'))
+            } else {
+                let member = {
+                    id: createID(),
+                    name: req.body.name
+                }
+                // ajout nouveau membre 
+                members.push(member)
+                res.json(success(member))
+            }
         } else {
-            let member = {
-                id: createID(),
-                name: req.body.name
-            }
-            // ajout nouveau membre 
-            members.push(member)
-            res.json(success(member))
+            res.json(error('no name value'))
         }
-    } else {
-        res.json(error('no name value'))
-    }
-})
+    })
+
+// on definit un middleware pour lier l'url /api/v1/members et le router MembersRouter
+app.use('/api/v1/members', MembersRouter)
 
 app.listen(8080, () => { console.log('Started on port 8080') })
 
