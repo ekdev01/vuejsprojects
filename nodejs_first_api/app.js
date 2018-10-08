335a55f58ba00d580126e7a4bdcc15259b2a18a3
ekdev01 <31770879+ekdev01@users.noreply.github.com>
2018-10-08 19:05:35 +0200

    Add url with parameter

--- nodejs_first_api/app.js
+++ nodejs_first_api/app.js
@@ -11,6 +11,10 @@ app.get('/api/v1', (req, res) => {
     res.send('API Version 1')
 })
 
+app.get('/api/v1/books/:id', (req, res) => {
+    res.send(req.params)
+})
+
 // Execution de l'application sur le port 8080 avec 
 // en callback un message dans la console
 app.listen(8080, () => { console.log('Started on port 8080') })
