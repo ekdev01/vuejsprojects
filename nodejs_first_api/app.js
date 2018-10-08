e70644a90f9a068a1e13ffc4e464d741f7a3e0bf
ekdev01 <31770879+ekdev01@users.noreply.github.com>
2018-10-08 21:30:31 +0200

    Add functions modules for our usuals functions

--- nodejs_first_api/app.js
+++ nodejs_first_api/app.js
@@ -1,7 +1,8 @@
 require('babel-register')
-const express = require('express')
-const app = express()
-const morgan = require('morgan')
+const express   = require('express')
+const app       = express()
+const morgan    = require('morgan')
+const func      = require('functions')
 
 // constante members qui contiendra tous les membres
 const members = [
@@ -24,34 +25,20 @@ app.use(morgan('dev'))
 // Retourne un membre a partir de l'id
 app.get('/api/v1/members/:id', (req, res) => {
     // Renvoie le membre correspondant a id.
-    res.json(success(members[(req.params.id) - 1]))
+    res.json(func.success(members[(req.params.id) - 1]))
 })
 
 // Renvoie tous les membres
 app.get('/api/v1/members', (req, res) => {
 
     if(req.query.max != undefined && req.query.max > 0) {
-        res.json(success(members.slice(0, req.query.max)))
-    } else if(req.query.max != undefined) {  // Cas d'erreur
-        res.json(error('Wrong max value'))
-    } else {
-        res.json(success(members))
+        res.json(func.success(members.slice(0, req.query.max)))
+    } 
+    else if(req.query.max != undefined) {  // Cas d'erreur
+        res.json(func.error('Wrong max value'))
+    } 
+    else {
+        res.json(func.success(members))
     }
 })
 app.listen(8080, () => { console.log('Started on port 8080') })
-
-// renvoie le statut success et le resultat
-function success(result) {
-    return {
-        status: 'success',
-        result: result
-    }
-}
-
-// renvoie le statut error et le message d'erreur
-function error(message) {
-    return {
-        status: 'error',
-        message: message
-    }
-}
