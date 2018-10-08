826494739664a69dc6e76741293e424fb7a9e728
ekdev01 <31770879+ekdev01@users.noreply.github.com>
2018-10-08 21:14:31 +0200

    Add the return status and result or error and message error

--- nodejs_first_api/app.js
+++ nodejs_first_api/app.js
@@ -24,16 +24,32 @@ app.use(morgan('dev'))
 // Retourne un membre a partir de l'id
 app.get('/api/v1/members/:id', (req, res) => {
     // Renvoie le membre correspondant a id.
-    res.json(members[(req.params.id) - 1])
+    res.json(success(members[(req.params.id) - 1]))
 })
 
 // Renvoie tous les membres
 app.get('/api/v1/members', (req, res) => {
 
     if(req.query.max != undefined && req.query.max > 0) {
-        res.json(members.slice(0, req.query.max))
+        res.json(success(members.slice(0, req.query.max)))
     } else {
-        res.json(members)
+        res.json(success(members))
     }
 })
 app.listen(8080, () => { console.log('Started on port 8080') })
+
+// renvoie le statut success et le resultat
+function success(result) {
+    return {
+        status: 'success',
+        result: result
+    }
+}
+
+// renvoie le statut error et le message d'erreur
+function error(message) {
+    return {
+        status: 'error',
+        message: message
+    }
+}
