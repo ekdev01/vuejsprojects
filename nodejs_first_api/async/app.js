7ea357ff0b090600d1c3b5cfb2812bcf38088e6c
ekdev01 <31770879+ekdev01@users.noreply.github.com>
2018-10-09 03:41:56 +0200

    Modification 3 of app.js to illustrate the use of callbacks

--- nodejs_first_api/async/app.js
+++ nodejs_first_api/async/app.js
@@ -3,11 +3,20 @@ require('babel-register')
 console.log('Debut')
 getMember( (member) => {
     console.log(member)
+    getArticles(member, (articles) => {
+        console.log(articles)
+    })
 })
 console.log('Fin')
 
 function getMember(next) {
     setTimeout(() => {
         next('Member 1') 
+    }, 1500)
+}
+
+function getArticles(member, next) {
+    setTimeout(() => {
+        next([1, 2, 3])
     }, 1500)
 }
