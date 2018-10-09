5ccc26a6b5e0c0dec2fa2cdb30e8d87b15491183
ekdev01 <31770879+ekdev01@users.noreply.github.com>
2018-10-09 05:09:32 +0200

    Promises in parallel

--- nodejs_first_api/async/app.js
+++ nodejs_first_api/async/app.js
@@ -2,27 +2,22 @@ require('babel-register')
 
 console.log('Debut')
 
-getMember()
-    .then(member => getArticles(member))
-    .then(articles => console.log(articles))
-    .catch(err => console.log(err.message))
-
-function getMember() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log('Member 1')
-            resolve('Member 1')
-        }, 1500)
+// promise one
+let p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('p1')
     })
-}
+})
 
-function getArticles(member) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            //resolve([1, 2, 3])
-            reject(new Error('Error during getMember...'))
-        }, 1500)
+// promise two
+let p2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('p2')
     })
-}
+})
+
+// mise en paralelle
+Promise.all([p1, p2])
+    .then(result => console.log(result))
 
 console.log('Fin')
